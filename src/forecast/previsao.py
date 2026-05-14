3a8cca2a8881208021233451fb581d23496bfd1b
VMantovaniKT <victormantovanibspb123@gmail.com>
2026-05-14 13:35:26 -0300

    feat: arquivo de previsão finalizado

--- src/forecast/previsao.py
+++ src/forecast/previsao.py
@@ -7,6 +7,8 @@ import numpy as np
 
 from modules.modulos import Colonia
 
+
+
 def historico_dados_eolica(): #Criação da função que irá armazenar os dados históricos de criação de energia eólica.
  
  """
@@ -16,8 +18,8 @@ def historico_dados_eolica(): #Criação da função que irá armazenar os dados
  """
  
  nome = "Eólica"
- dia_de_teste = [1, 2, 3, 4, 5, 6]
- velocidade_vento = [28, 12, 8, 16, 20, 18] #<- velocidade máxima 30 m/s
+ 
+ velocidade_vento = [28,   12,   8,    16,    20,   18] #<- velocidade máxima 30 m/s
  energia_gerada = [18.48, 7.92, 5.28, 10.56, 13.2, 11.88] # <- a cada 1 m/s de vento, a energia gerada é de 0.66 (20/30) da velocidade do vento, ou seja, 0.66 * velocidade_vento = energia_gerada.
 
  """
@@ -25,7 +27,6 @@ def historico_dados_eolica(): #Criação da função que irá armazenar os dados
  Mas ainda é coerente com as regras do Aelton, visto que ele apenas limita o máximo do armazenamento em 20 (int), e não ultrapassa esse limite.
  """
  return  nome, velocidade_vento, energia_gerada
-
 
 
 def historico_dados_solar(): #Criação da função que irá armazenar os dados históricos de criação de energia solar.
@@ -37,12 +38,11 @@ def historico_dados_solar(): #Criação da função que irá armazenar os dados 
  """
 
  nome = "Solar"
- dia_de_teste = [1, 2, 3, 4, 5, 6]
- radiacao_solar = [90, 70, 50, 80, 100, 60] # <- radiação solar máxima 100 
- energia_gerada = [27.00, 21.00, 15.00, 24.00, 30.00, 18.00] # <- a cada 1% de radiação solar, a energia gerada é de 0.3 (30/100) da radiação solar, ou seja, 0.3 * radiacao_solar = energia_gerada.
+ radiacao_solar = [90,     70,     50,    80,   30,   60] # <- radiação solar máxima 100 
+ energia_gerada = [27.00, 21.00, 15.00, 24.00, 9.00, 18.00] # <- a cada 1% de radiação solar, a energia gerada é de 0.3 (30/100) da radiação solar, ou seja, 0.3 * radiacao_solar = energia_gerada.
 
  return  nome, radiacao_solar, energia_gerada
-
+ 
 
 def regressao_eolica():
 
@@ -53,6 +53,7 @@ def regressao_eolica():
 
  return coeficientes[0], coeficientes[1] # <- retorna os coeficientes a e b
 
+
 def regressao_solar():
  
  nome, radiacao_solar, energia_gerada = historico_dados_solar() 
@@ -60,3 +61,61 @@ def regressao_solar():
  coeficientes = np.polyfit(radiacao_solar, energia_gerada, 1)
 
  return coeficientes[0], coeficientes[1]
+
+
+def previsao_energia_eolica(velocidade_vento): #Criação da função que irá prever a quantidade de energia eólica gerada a partir da velocidade do vento, utilizando os coeficientes a e b obtidos na regressão linear.
+
+ a, b = regressao_eolica()
+
+ energia_gerada = a * velocidade_vento + b
+ return energia_gerada
+
+
+def previsao_energia_solar(radiacao_solar): #Criação da função que irá prever a quantidade de energia solar gerada a partir da radiação solar, utilizando os coeficientes a e b obtidos na regressão linear.
+
+ a, b = regressao_solar()
+
+ energia_gerada = a * radiacao_solar + b
+
+ return energia_gerada
+
+
+def plotar_grafico_eolica():
+ nome, velocidade_vento, energia_gerada = historico_dados_eolica()
+ a, b = regressao_eolica()
+
+
+ x_reta = np.linspace(0, 30, 100) # <- Cria um espaçamento de 100 pontos para o gráfico ficar mais legível e estético 
+ y_reta = a * x_reta + b # <- Calcula os valores de Y (energia gerada) para cada valor de X (velocidade do vento) utilizando a equação da reta de regressão linear.
+
+
+ plt.scatter(velocidade_vento, energia_gerada, zorder=3) # <- Declara o eixo X (causa) e o eixo Y (efeito) 
+ plt.plot(x_reta, y_reta, zorder=2) # <- Plota a reta de regressão linear utilizando os dados históricos de criação de energia eólica.
+ for x, y in zip(velocidade_vento,energia_gerada):
+  plt.annotate(f"{y}kWh", (x,y), textcoords="offset points",fontsize = 6, xytext=(0,10), ha='center') # <- Adiciona anotações aos pontos do gráfico para mostrar o valor de energeria gerada (y = causa).
+ plt.xlabel("Velocidade do Vento (m/s)") # Nomeia o eixo X
+ plt.ylabel("Energia Gerada (unidades)") # Nomeia o eixo Y
+ plt.title(f"Energia {nome} Gerada vs Velocidade do Vento") # Nomeia o gráfico
+ plt.grid() # <- Adiciona uma grade ao gráfico
+ plt.show() # <- Exibe o gráfico
+
+
+def plotar_grafico_solar():
+ nome, radiacao_solar, energia_gerada = historico_dados_solar()
+ a, b = regressao_solar()
+
+ x_reta = np.linspace(0, 100, 100) # <- Cria um espaçamento de 100 pontos para o gráfico ficar mais legível e estético 
+ y_reta = a * x_reta + b # <- Calcula os valores utilizando a fórmula da regressão linear grau 1 
+
+ plt.scatter(radiacao_solar, energia_gerada, zorder = 3) # <- Declara o eixo X (causa) e o eixo Y (efeito) 
+ plt.plot(x_reta, y_reta, zorder = 2) # <- Plota a reta de regressão linear utilizando os dados históricos de criação de energia solar.
+ for x, y in zip(radiacao_solar, energia_gerada):
+  plt.annotate(f"{y}kWh", (x,y), textcoords="offset points",fontsize = 6, xytext=(0,10), ha='center')
+ plt.xlabel("Radiação Solar (%)") # Nomeia o eixo X
+ plt.ylabel("Energia Gerada (unidades)") # Nomeia o eixo Y
+ plt.title(f"Energia {nome} Gerada vs Radiação Solar") # Nomeia o gráfico
+ plt.grid() # <- Adiciona uma grade ao  gráfico
+ plt.xticks(range(0,101,5))
+ plt.show() # <- Exibe o gráfico
+
+ 
